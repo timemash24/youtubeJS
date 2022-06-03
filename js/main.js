f1a13604b7edfd84ac739fca84ed4b561c201138
Timemash <timemash24@gmail.com>
2022-06-03 15:45:10 +0900

    Update style.css Update main.js for action buttons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
+const actionBtns = document.querySelectorAll('.actions button i');
+
+function handleActionBtn(event) {
+  const btn = event.target;
+  btn.classList.toggle('active');
+}
 
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
+});
+
+actionBtns.forEach((btn, i) => {
+  if (i <= 1) btn.addEventListener('click', handleActionBtn);
 });
